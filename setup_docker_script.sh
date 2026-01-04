6b22ff1f2808178293f9f62272c5259093ed4f30
Ubuntu <ubuntu@ip-172-31-11-26.ap-south-1.compute.internal>
2026-01-04 16:15:52 +0000

    makeup file added and docker script

---
+++ setup_docker_script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+echo "Installing Docker..."
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+sudo tee /etc/apt/sources.list.d/docker.sources > /dev/null <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+echo "Docker installation complete..."
+
+echo "Setting permission to user for docker sudo"
+sudo usermod -aG docker $USER
+newgrp docker
+echo "Done..."
